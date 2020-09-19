2c3d5a6aa732a56f4c5c0da0383f16885664aef9
David Guadalupe Vargas Lopez <david.vargas7480@alumnos.udg.mx>
2020-09-19 16:06:01 -0500

    ademas pide la edad

--- main.c
+++ main.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 
 int main(){
+    char nombre;
+    unsigned int edad;
     printf("hola mundo\n");
+    printf("nombre:");
+    scanf("%s",&nombre);
+    scanf("edad: %u",&edad);
+    printf("edad:%u",edad);
+  
+    
+
     return 0;
 }
